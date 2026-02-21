e9ab18b21c973ed943435e9713785e113fcd2b37
Luvaatym <telyatnikmaria08@icloud.com>
2026-02-22 01:42:21 +0300

    Task 3: Определение количества вещественных корней квадратного уравнения

--- Program.cs
+++ Program.cs
@@ -17,12 +17,26 @@ public class Program
 
         return hundreds * 100 + ones;
     }
-}
-public static string GetChessCellColor(int x, int y)
-{
-    if (x < 1 || x > 8 || y < 1 || y > 8)
-        throw new ArgumentOutOfRangeException("Координаты должны быть от 1 до 8.");
 
-    return (x + y) % 2 == 0 ? "Черная" : "Белая";
+    public static string GetChessCellColor(int x, int y)
+    {
+        if (x < 1 || x > 8 || y < 1 || y > 8)
+            throw new ArgumentOutOfRangeException("Координаты должны быть от 1 до 8.");
+
+        return (x + y) % 2 == 0 ? "Черная" : "Белая";
+    }
+    /*Даны координаты поля шахматной доски x, y (целые числа, лежащие в диапазоне 1–8). Учитывая, что левое нижнее поле доски (1, 1) является черным, вывести, какой цвет имеет поле с данными координатами.*/
+    public static int CountRealRoots(double a, double b, double c)
+    {
+        if (a == 0)
+            throw new ArgumentException("A не может быть 0.");
+
+        double d = b * b - 4 * a * c;
+
+        if (d > 0) return 2;
+        if (d == 0) return 1;
+        return 0;
+    }
+    /*Даны числа 𝐴, 𝐵, 𝐶 (число 𝐴 не равно 0). Вернуть количество вещественных корней квадратного уравнения 𝐴𝑥2+𝐵𝑥+𝑐=0.*/
+
 }
-/*Даны координаты поля шахматной доски x, y (целые числа, лежащие в диапазоне 1–8). Учитывая, что левое нижнее поле доски (1, 1) является черным, вывести, какой цвет имеет поле с данными координатами.*/
