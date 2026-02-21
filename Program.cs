ec84e9e78ac0fe8e593a02bfc4e1d30d8f5e4bef
Luvaatym <telyatnikmaria08@icloud.com>
2026-02-22 01:36:46 +0300

    Task 1: Обнуление разряда десятков

--- Program.cs
+++ Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+    }
+
+    /*Дано трехзначное число. Обнулить в нем разряд десятков.*/
+    public static int RemoveTensDigit(int number)
+    {
+        if (number < 100 || number > 999)
+            throw new ArgumentException("Число должно быть трехзначным.");
+
+        int hundreds = number / 100;
+        int ones = number % 10;
+
+        return hundreds * 100 + ones;
+    }
+}
